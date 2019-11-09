e24d89bc721142036480fbfd210c146674cb2335
Jacob Angulo <jacobangulo@hotmail.com>
2019-11-09 14:54:38 -0800

    rjnnsncov

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 const server = require("./server");
 
-server.listen(4000, (req, res) => {
+server.listen(process.env.PORT || 4000, (req, res) => {
   console.log("sever running on port 4000");
 });
